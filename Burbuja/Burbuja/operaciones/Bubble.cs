c65ab23a5ee6308debc24d17cadc80976d150aca
Garu006 <garojas@uamv.edu.ni>
2024-09-30 08:11:40 -0600

    Sesion 13 de clase

---
+++ Burbuja/Burbuja/operaciones/Bubble.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Burbuja.operaciones
+{
+    internal class MetBurbuja
+    {
+       //metodo burbuja consiste en comparar dos valores 
+       public void ordenarBurbuja(int[] arreglo)
+       {
+            for(int i = 0; i <= arreglo.Length; i++)
+            {
+                for(int j=i+1; j< arreglo.Length; j++)
+                {
+                    if (arreglo[i] < arreglo[i])
+                    {
+                        int temp = arreglo[i];
+                        arreglo[j] = arreglo[i];
+                        arreglo[i] = temp;
+                    }
+                }
+            }
+       }
+    }
+}
